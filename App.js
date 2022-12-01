4d550cb81d2ed5e908d69b9f2b626477c120c22a
i21s590 <i21s590@students.it-college.ru>
2022-12-01 19:30:24 +0500

    первоначальная вёрстка главной страницы

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
+
 
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Sms service</Text>
+      <TextInput style={styles.input} placeholder='Enter phone number' />
+      <TextInput style={styles.input} placeholder='Enter a message' />
+
+      <Button title="Send message"/>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +24,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    borderRadius: 5,
+    padding: 8,
+    margin: 10,
+    width: 200,
+  },
+  text: {
+    fontSize: 20,
+  }
 });
